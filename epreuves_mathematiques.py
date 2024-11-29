3be69c96f670f1f8489da823e4194f79602ad1de
JeanChipo <max.lef@free.fr>
2024-11-29 16:07:43 +0100

    Ajout du jeu de la roulette mathématique

--- epreuves_mathematiques.py
+++ epreuves_mathematiques.py
@@ -1,1 +1,33 @@
+from random import randint, choice
 
+def roulette_mathematique(joueur:str)->None:
+    """
+    :param joueur: nom du joueur
+    :return:
+    """
+    nombres = [randint(1,20) for _ in range(5)]
+    op = choice(['+','-','*'])
+    print(f"Nombres sur la roulette : {nombres}")
+
+    match op:
+        case '+':
+            result = 0
+            for n in nombres:
+                result += n
+            print(f"{joueur}, calculez le résultat en combinant ces nombres avec une addition")
+        case '-':
+            result = 0
+            for n in nombres:
+                result -= n
+            print(f"{joueur}, calculez le résultat en combinant ces nombres avec une soustraction")
+        case '*':
+            result = 1
+            for n in nombres:
+                result *= n
+            print(f"{joueur}, calculez le résultat en combinant ces nombres avec une multiplication")
+        case _:
+            result = -1
+
+    reponse = int(input("Votre réponse : "))
+    if reponse == result: print("Bonne réponse! Vous avez gagné une clé.")
+    else: print("Mauvaise réponse! Vous avez perdu la clé.")
