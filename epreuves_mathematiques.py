b15f856c6c5248115cf63beddf396c51b211f4ca
JeanChipo <max.lef@free.fr>
2024-12-18 15:20:04 +0100

    résolution des merges errors + fix fonction est_décimal + fix fonction roulette_mathematique

--- epreuves_mathematiques.py
+++ epreuves_mathematiques.py
@@ -34,8 +34,6 @@ def est_decimal(n:str)->bool:
             if not(ord('0') <= ord(e) <= ord('9')) or nb_de_points > 1:
                 decimal = False
                 break
-    if nb_de_points == 1:
-        decimal = False
     return decimal
 
     ################################
@@ -57,9 +55,9 @@ def epreuve_math_factorielle()->bool:
     :return: True si une clé est gagnée, False sinon
     """
     n = randint(1,10)
-    reponse = input(f"Calculer la factorielle de {n}: ")
+    reponse = input(f"Calculer la factorielle de {n} : ")
     while not est_entier(reponse):
-        reponse = input("Merci de saisir un entier: ")
+        reponse = input("Merci de saisir un entier : ")
     if int(reponse) == factorielle(n):
         print("Correct! Vous gagnez une clé.")
         return True
@@ -84,7 +82,7 @@ def epreuve_math_equation()->bool:
     (a,b,x) = resoudre_equation_lineaire()
     reponse = input(f"Résoudre l'équation suivante (si c'est une fraction arrondir à 2 chiffres après la virgule) : {a}x + {b} = 0 : ")
     while not est_decimal(reponse):
-        reponse = input("Merci de saisir un entier: ")
+        reponse = input("Merci de saisir un entier : ")
     if float(reponse) == float(x):
         print("Correct! Vous gagnez une clé.")
         return True
@@ -123,21 +121,11 @@ def epreuve_math_premier()->bool:
     :return: True si une clé est gagnée, False sinon
     """
     n = randint(10,20)
-    print(f"Épreuve de Mathématiques: Trouver le nombre premier le plus proche de {n}")
+    print(f"Épreuve de Mathématiques: Trouver le nombre premier le plus proche de {n}.")
 
-    reponse = input("Votre réponse: ")
+    reponse = input("Votre réponse : ")
     while not est_entier(reponse):
-        reponse = input("Merci de saisir un entier: ")
-#    if int(reponse) == premier_plus_proche():
-#
-#    while True:
-#        reponse = input("Votre réponse: ")
-#
-#        if est_entier(reponse):
-#            reponse = int(reponse)
-#            break   # réponse valide
-#        else:   # réponse non valide
-#          print("Saisie incorrect")
+        reponse = input("Merci de saisir un entier : ")
     if int(reponse) == premier_plus_proche(n):
         print("Correct! Vous gagnez une clé.")
         return True
@@ -155,7 +143,7 @@ def roulette_mathematique()->bool:
     """
     nombres = [randint(1,20) for _ in range(5)]
     op = choice(['+','-','*'])
-    print(f"Nombres sur la roulette : {nombres}")
+    print(f"Nombres sur la roulette : {nombres}.")
 
     match op:
         case '+':
@@ -167,31 +155,30 @@ def roulette_mathematique()->bool:
             result = 0
             for n in nombres:
                 result -= n
-            print("Calculez le résultat en combinant ces nombres avec une addition.")
+            print("Calculez le résultat en combinant ces nombres avec une soustraction.")
         case '*':
             result = 1
             for n in nombres:
                 result *= n
-            print("Calculez le résultat en combinant ces nombres avec une addition.")
+            print("Calculez le résultat en combinant ces nombres avec une multiplication.")
         case _:
             result = -1
 
     reponse = input("Votre réponse : ")
-    if est_entier(reponse) and int(reponse) == result:
+    while not est_entier(reponse):
+        reponse = input("Merci de saisir un entier : ")
+    if int(reponse) == result:
         print("Correct! Vous gagnez une clé.")
         return True
     print("Faux! Vous perdez la clé.")
     return False
 
-    #####################################################
-    ## Fonction epreuve math (selection d'une épreuve  ##
-    #####################################################
+    ######################################################
+    ## Fonction epreuve math (selection d'une épreuve)  ##
+    ######################################################
 
 def epreuve_math()->None:
     """ Execute une fonction aléatoire du module epreuves_mathematiques """
     epreuves = [epreuve_math_factorielle, epreuve_math_equation, epreuve_math_premier, roulette_mathematique]
     epreuve = epreuves[randint(0,3)]
     epreuve()
-
-while True:
-    epreuve_math()
