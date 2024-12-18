1a18b1bc0db13f7574dbc4bba2bb60effd619900
JeanChipo <max.lef@free.fr>
2024-12-18 14:37:32 +0100

    Ajout des fonctions de vérifications dans le fichier. -> module fini !

--- epreuves_mathematiques.py
+++ epreuves_mathematiques.py
@@ -1,5 +1,42 @@
 from random import randint, choice
-from fonctions_utiles import est_entier, est_decimal
+
+    ################################
+    ## Fonctions de vérification  ##
+    ################################
+
+def est_entier(n:str)->bool:
+    """
+    :param n: une chaine de caractères
+    :return: True si n est entier, False sinon
+    """
+    if n == '': return False    # cas où on fait un retour à la ligne (caractère de taille 0)
+    if n[0] == '-': n = n[1:]   # cas où n est négatif, on enleve le 1er caractère ('-')
+    entier = True
+    for e in n:
+        if not(ord('0') <= ord(e) <= ord('9')):
+            entier = False
+            break
+    return entier
+
+def est_decimal(n:str)->bool:
+    """
+    :param n: une chaine de caractères
+    :return: True si n est decimal, False sinon
+    """
+    if n == '': return False    # cas où on fait un retour à la ligne (caractère de taille 0)
+    if n[0] == '-': n = n[1:]   # cas où n est négatif, on enleve le 1er caractère ('-')
+    decimal = True
+    nb_de_points = 0
+    for e in n:
+        if e == '.':    # cas où n est un décimal
+            nb_de_points += 1
+        else:
+            if not(ord('0') <= ord(e) <= ord('9')) or nb_de_points > 1:
+                decimal = False
+                break
+    if nb_de_points == 1:
+        decimal = False
+    return decimal
 
     ################################
     ## Épreuve de la factorielle  ##
